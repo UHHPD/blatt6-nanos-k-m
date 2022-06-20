6c04fe6adfa305c54e7c9b35e694ff44b851937c
Karim <102976710+KAnndas@users.noreply.github.com>
2022-06-20 19:42:21 +0000

    Übungsblatt 6 erledigt

--- Vollzylinder.cc
+++ Vollzylinder.cc
@@ -6,14 +6,17 @@
 Vollzylinder::Vollzylinder(double nr, double nl) {
   r_ = nr;
   l_ = nl;
+  k_name = "Vollzylinder";
+  k_masse = 1;
 }
 
 Vektor Vollzylinder::punkt() {
-  double R = 0;
+  double R = r_;
   double phi = 2 * M_PI * rand() / (double)RAND_MAX;
   double z = l_ * (rand() / (double)RAND_MAX - 0.5);
+  double r = R*sqrt(rand()/(double)RAND_MAX); 
 
-  double x = 0;
-  double y = 0;
+  double x = r*cos(phi);
+  double y = r*sin(phi);
   return Vektor(x, y, z);
 };
